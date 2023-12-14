db7a0cf56a6ed499c38ca9979a2c4bad1eebb18c
Gabriel110 <gabrielcorreiadev@gmail.com>
2023-12-13 21:02:30 -0300

    help

--- Docker/desafio-02/src/db.js
+++ Docker/desafio-02/src/db.js
@@ -1,13 +1,15 @@
 const { Sequelize } = require("sequelize"); 
 
-const dbName = 'peaple';
+const dbName = 'people';
 const dbUser = 'root';
-const dbHost = 'localhost:3306';
+const dbHost =  'host.docker.internal';
+const port  = '3306';
 const dbPassword = 'password';
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   dialect: "mysql", 
-  host: dbHost, 
+  host: dbHost,
+  port: port
 });
 
 module.exports = sequelize;
